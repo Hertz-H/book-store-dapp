de0dc53271fa351d05c8719438ddfca2562599c6
Afaf Albakri <afaf14albakri@gmail.com>
2025-05-31 12:34:09 +0300

    fetched data as book

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
+export const dynamic = "force-dynamic"; // Add this line
 import { headers } from "next/headers"; // added
 import ContextProvider from "@/context";
 
